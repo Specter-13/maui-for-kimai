d822ae19a3bc6d50dec6876d29ae0c8955f51543
Dávid Špavor <xspavo00@vutbr.cz>
2023-04-18 10:07:54 +0200

    possible fix for duplicate for build error

--- src/MauiForKimai.App/Platforms/Android/MainApplication.cs
+++ src/MauiForKimai.App/Platforms/Android/MainApplication.cs
@@ -3,12 +3,8 @@ using Android.Runtime;
 
 namespace MauiForKimai;
 
-#if DEBUG                                   // connect to local service on the
-[Application(UsesCleartextTraffic = true)]  // emulator's host for debugging,
-#else                                       // access via http://10.0.2.2
-[Application]                               
-#endif
-//[Application]   
+
+[Application(UsesCleartextTraffic = true)]  
 public class MainApplication : MauiApplication
 {
 	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
